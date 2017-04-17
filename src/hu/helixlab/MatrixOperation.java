d16f9b2c01988a0b309908b07c1aa22718d59505
wolfika222 <farkas0528@gmail.com>
2017-04-17 11:53:31 +0200

    #2

--- src/hu/helixlab/MatrixOperation.java
+++ src/hu/helixlab/MatrixOperation.java
@@ -8,10 +8,10 @@ import java.util.Random;
 public class MatrixOperation implements IMatrixOperation {
 
     public void printMatrix(int matrix[][]) {
-        Random random = new Random();
+        //Random random = new Random();
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                matrix[i][j] = random.nextInt(2);
+                //matrix[i][j] = random.nextInt(2);
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();

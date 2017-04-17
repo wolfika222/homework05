e362681a850980af0eb69edb911b4c98b5c50f65
wolfika222 <farkas0528@gmail.com>
2017-04-17 11:44:18 +0200

    #1

--- src/hu/helixlab/ISort.java
+++ src/hu/helixlab/ISort.java
@@ -6,4 +6,9 @@ package hu.helixlab;
 public interface ISort {
 
     void bubbleSort(int array[]);
+    void insertionSort(int array[]);
+    void selectionSort(int array[]);
+    void mergeSort(int array[]);
+    void quickSort(int array[]);
+
 }

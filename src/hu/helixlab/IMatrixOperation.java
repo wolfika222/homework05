aa6f71512dae2382d556c5c93622bceca6b7dbac
wolfika222 <farkas0528@gmail.com>
2017-04-22 22:01:18 +0200

    #6/3

--- src/hu/helixlab/IMatrixOperation.java
+++ src/hu/helixlab/IMatrixOperation.java
@@ -7,4 +7,6 @@ public interface IMatrixOperation {
 
     void printMatrix(int matrix[][]);
     int[][] initMatrixRandomNumbers(int n, int m, int min, int max);
+    int[][] multiplyWithScalar(int scalar, int matrix[][]);
+    int[][] sum(int matrix[][], int matrix1[][]);
 }

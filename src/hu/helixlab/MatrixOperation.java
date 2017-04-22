253ee61b975eb8dd8ba72f85e59482c34331bc87
wolfika222 <farkas0528@gmail.com>
2017-04-22 22:09:14 +0200

    #7/4

--- src/hu/helixlab/MatrixOperation.java
+++ src/hu/helixlab/MatrixOperation.java
@@ -30,4 +30,19 @@ public class MatrixOperation implements IMatrixOperation {
         }
         return mat;
     }
+
+    public int[][] multiplyWithScalar(int scalar, int matrix[][]){
+
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int j = 0; j < matrix[i].length ; j++) {
+                matrix[i][j] = matrix[i][j] * scalar;
+            }
+        }
+        return matrix;
+    }
+
+    public int[][] sum(int matrix[][], int matrix1[][]){
+        int[][] mat = new int[matrix.length][matrix.length];
+        return mat;
+    }
 }
